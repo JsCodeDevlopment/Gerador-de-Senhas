ecc152a194d1ed3e73bbf63286512b9f59586640
Jonata Silva <jonatasilva118@gmail.com>
2023-06-25 16:33:42 -0300

    Add Input

--- src/Components/Generator.tsx
+++ src/Components/Generator.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 export function Generator() {
   const [generatedKey, setGenerationKey] = useState("Senha aqui!");
   const [copied, setCopied] = useState("Copiar");
+  const [passwordSize, setPasswordSize] = useState(8);
 
   const characterSet =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?`~";
-  const keyLength = 8;
 
   function GenerateKey() {
     let key = "";
-    for (let i = 0; i < keyLength; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const randomIndex = Math.floor(Math.random() * characterSet.length);
       key += characterSet.charAt(randomIndex);
     }
@@ -25,15 +25,28 @@ export function Generator() {
   return (
     <div className="flex flex-col bg-zinc-950 w-full items-center gap-5 justify-center h-screen">
       <h1 className="text-stone-50 text-3xl">Gerador de Senhas</h1>
+      <input
+        className="text-purple-950 p-2 rounded-md border-purple-950"
+        type="number"
+        id="passwordSize"
+        min={1}
+        max={16}
+        value={passwordSize}
+        placeholder="Qnt. Caracteres"
+        onChange={(ev) => setPasswordSize(ev.target.value)}
+      />
+      <p className="text-white">Qnt. de Caracteres!</p>
       <div className="flex gap-12">
         <button
           className="bg-purple-950 text-white p-2 rounded-md"
-          onClick={GenerateKey}>
+          onClick={GenerateKey}
+        >
           Gerar!
         </button>
         <button
           className="bg-purple-950 text-white p-2 rounded-md"
-          onClick={Copy}>
+          onClick={Copy}
+        >
           {copied}
         </button>
       </div>
